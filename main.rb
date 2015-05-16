bed44a368f69ddb8bb68d45e8090f0108b18d2be
Peter Lauck <griest024@gmail.com>
2015-05-16 14:41:14 -0400

    loads yaml file and displays first set of instance variable in treeview

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'gtk2'
+require_relative './lib/rgss'
 require 'require_all'
 
 #make program output in real time so errors visible in VR.
@@ -8,6 +9,7 @@ STDOUT.sync = true
 STDERR.sync = true
 
 $plugins = {}
+$YAMLDir = '../Pokemon-Virginia/src/Data'
 
 def add_plugin(plugin_name, plugin_class)
 	$plugins[plugin_name] = plugin_class
--- plugins/editor-data.rb
+++ plugins/editor-data.rb
@@ -1,15 +1,37 @@
 
+require 'yaml'
 
 class DataEditor
 
-	def initialize
+	def initialize(single_file = false)
 		createGUI
 	end
 
 	def createGUI
 		@window = Gtk::Window.new
-		
+		@treeStore = Gtk::TreeStore.new(String)
+		YAMLtoTree()
+		@treeView = Gtk::TreeView.new(@treeStore)
+		rend = Gtk::CellRendererText.new
+		col = Gtk::TreeViewColumn.new("Data", rend, :text => 0)
+		@treeView.append_column(col)
+		@window.add(@treeView)
 		@window.show_all
+	end
+
+	def YAMLtoTree
+		parsed = begin
+  			YAML::load(File.open("#{$YAMLDir}/Map077.yaml"))
+		rescue ArgumentError => e
+  			puts "Could not parse YAML: #{e.message}"
+		end
+		@map = parsed["root"]
+		File.open("dbg.txt", "w") { |file| file.puts @map.instance_variables[0].class }
+		@map.instance_variables.each do |e|
+			a = @treeStore.append(nil)
+			a[0] = e
+		end
+
 	end
 end
 
