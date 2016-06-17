897192c0ffc1a288b70262164495eba9cb79055b
Peter Lauck <griest024@gmail.com>
2016-06-17 17:52:15 -0400

    wrappers have no overridden class, fixes load problems

--- lib/pkmnee/util/wrapper.rb
+++ lib/pkmnee/util/wrapper.rb
@@ -3,10 +3,8 @@ module PKMNEE::Util
 	class DataWrapper
 
 		attr_accessor :path, :id
-		attr_reader :class
 
 		def initialize(klass, path)
-			@class = klass
 			@path = path
 			@id = File.basename(path, ".*").to_sym
 		end
@@ -24,12 +22,18 @@ module PKMNEE::Util
 
 		def initialize(path)
 			@path = path
-			@class = PKMNEE::Util::TileImageWrapper
 			@id = File.basename(path, ".*")
 		end
 		
 		def load
 			JavaFX::Image.new(resource_url(:tiles, path).to_s)
+		end
+	end
+
+	class AutotileImageWrapper < TileImageWrapper
+
+		def load
+			JavaFX::Image.new(resource_url(:autotiles, path).to_s)
 		end
 	end
 end
